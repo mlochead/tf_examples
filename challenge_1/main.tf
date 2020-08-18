c7e70d9520adbce716e871ac17fe7c352b5a6f4a
mlochead <mlochead@gmail.com>
2020-08-18 07:00:49 -0600

    ‘update’

---
+++ challenge_1/main.tf
@@ -0,0 +1,122 @@
+provider "aws" {
+  region     = var.region
+}
+
+data "aws_ami" "the_ami" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = [ "ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*" ]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  owners = [ "099720109477" ]
+}
+
+resource "aws_vpc" "vpc1" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "vpc1"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.vpc1.id
+  cidr_block = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "subnet1"
+  }
+}
+
+resource "aws_security_group" "allow_ssh_1" {
+  name        = "allow_ssh_1"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.vpc1.id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.vpc1.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_1"
+  }
+}
+
+resource "aws_security_group" "allow_ssh_2" {
+  name        = "allow_ssh_2"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.vpc2.id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.vpc1.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_2"
+  }
+}
+
+resource "aws_vpc" "vpc2" {
+  cidr_block       = "10.0.0.0/24"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "vpc2"
+  }
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id     = aws_vpc.vpc1.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "subnet2"
+  }
+}
+
+resource "aws_instance" "instance1" {
+  ami                    = data.aws_ami.the_ami.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.subnet1.id
+  security_groups        = aws_security_group.allow_ssh_1.id
+ }
+
+ resource "aws_instance" "instance2" {
+   ami                    = data.aws_ami.the_ami.id
+   instance_type          = var.instance_type
+   subnet_id              = aws_subnet.subnet2.id
+   security_groups        = aws_security_group.allow_ssh_2.id
+  }
---
+++ challenge_1/variables.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  description = "Region"
+}
+
+variable "instance_type" {
+  description = "Instance Type"
+}
