5946eb30ac76d78388e9cf1ef0bc41aeb4ce1962
mlochead <mlochead@gmail.com>
2020-08-19 10:51:13 -0600

    ‘update’

--- challenge_1/main.tf
+++ challenge_1/main.tf
@@ -103,6 +103,49 @@ data "aws_security_group" "default2" {
   vpc_id = aws_vpc.vpc2.id
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc1.id
+  tags = {
+    Name = "igw"
+  }
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rt_assoc" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_internet_gateway" "igw2" {
+  vpc_id = aws_vpc.vpc2.id
+
+  tags = {
+    Name = "igw2"
+  }
+}
+
+resource "aws_route_table" "route_table2" {
+  vpc_id = aws_vpc.vpc2.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw2.id
+  }
+}
+
+resource "aws_route_table_association" "rt_assoc2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.route_table2.id
+}
+
 resource "aws_instance" "instance1" {
   ami                    = data.aws_ami.the_ami.id
   instance_type          = var.instance_type
